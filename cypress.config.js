2fe8eff12d6d48a14b3289ad93840e9f26059fd7
Adrian Allen <118277671+AAllen83@users.noreply.github.com>
2023-12-11 12:37:48 -0500

    Integration to e2e & typescript added

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 600,
+  viewportHeight: 800,
+  defaultCommandTimeout: 4000,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:3000',
+  },
+})
